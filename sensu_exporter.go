6ee4795d1331f11ee79b90d9a9d025938998b226
reachlin <reachlin@gmail.com>
2017-04-28 17:37:12 +0800

    fix

--- sensu_exporter.go
+++ sensu_exporter.go
@@ -73,7 +73,7 @@ func getSensuResults(url string) error {
 		return err
 	}
 	for i, result := range results {
-		log.Infoln("...", fmt.Sprintf("%d, %v", i, result.Name))
+		log.Infoln("...", fmt.Sprintf("%d, %v", i, result.Check.Name))
 	}
 	return nil
 }
